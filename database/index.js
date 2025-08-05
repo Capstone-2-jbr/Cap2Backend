5b221125de7b80160f4f53c39a1a044fb17a834b
tabularasae <tabularasae@icloud.com>
2025-08-05 13:05:11 -0400

    Post table schema

--- database/index.js
+++ database/index.js
@@ -1,9 +1,11 @@
 const db = require("./db");
 const User = require("./user");
 const UserSetting = require("./user_settings.js")
+const Post = require("./posts")
 
 module.exports = {
   db,
   User,
   UserSetting,
+  Post,
 };
