91a3e8f77a19709abe1ef655b2af1f280da34c38
tabularasae <tabularasae@icloud.com>
2025-08-05 13:07:53 -0400

    PostMedia table

--- database/index.js
+++ database/index.js
@@ -2,10 +2,12 @@ const db = require("./db");
 const User = require("./user");
 const UserSetting = require("./user_settings.js")
 const Post = require("./posts")
+const PostMedia = require("./post_media.js")
 
 module.exports = {
   db,
   User,
   UserSetting,
   Post,
+  PostMedia,
 };
