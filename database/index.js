0bb89f5f5fe2a195351e632b21210fb4650bc802
tabularasae <tabularasae@icloud.com>
2025-08-05 13:10:12 -0400

    Comments table

--- database/index.js
+++ database/index.js
@@ -3,6 +3,7 @@ const User = require("./user");
 const UserSetting = require("./user_settings.js")
 const Post = require("./posts")
 const PostMedia = require("./post_media.js")
+const Comment = require("./comments.js")
 
 module.exports = {
   db,
@@ -10,4 +11,5 @@ module.exports = {
   UserSetting,
   Post,
   PostMedia,
+  Comment,
 };
