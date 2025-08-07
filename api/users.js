9872ef518cb2f5b6d8fc2c735659a3b2b3cc8e4d
tabularasae <tabularasae@icloud.com>
2025-08-07 12:58:07 -0400

    added the patch route to user

--- api/users.js
+++ api/users.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const axios = require("axios");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const { User, UserSetting, Playlist, PlaylistItem, Listing, ListingMedia } = require("../database");
+const {
+  User,
+  UserSetting,
+  Playlist,
+  PlaylistItem,
+  Listing,
+  ListingMedia,
+} = require("../database");
 
 router.get("/", async (_req, res) => {
   try {
@@ -26,8 +34,6 @@ router.get("/", async (_req, res) => {
     res.status(500).send("Error fetching Users: ");
   }
 });
-
-module.exports = router;
 
 router.get("/:id", async (req, res) => {
   try {
@@ -58,3 +64,27 @@ router.get("/:id", async (req, res) => {
     res.status(500).send("Error fetching User by ID");
   }
 });
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      include: [{ model: UserSetting, as: "settings" }],
+    });
+    if (!user) return res.sendStatus(404);
+    const { settings, password, ...rest } = req.body;
+    await user.update({
+      ...rest,
+      ...(password && { password_hash: bcrypt.hashSync(password, 10) }),
+    });
+    if (settings)
+      if (user.settings) await user.settings.update(settings);
+      else await UserSetting.create({ ...settings, user_id: user.user_id });
+    const plain = user.get({ plain: true });
+    delete plain.password_hash;
+    res.json(plain);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
