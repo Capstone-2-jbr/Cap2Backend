55cf80c27fdd365a0c0ea8f88fb031f2498c6e99
JoseG2704 <joseg50274@gmail.com>
2025-08-18 14:19:00 -0400

    Added more routing and potential bug fix

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const youtubeRouter = require("./youtube");  // <-- Import youtube router
-const playlistRouter = require("./playlist");
+const playlistRouter = require("./playlists");
 const userRouter = require("./users");
+const { authenticateJWT } = require("../auth");
 
 router.use("/video-details", youtubeRouter);
-router.use("/playlist", playlistRouter);
-router.use("/users", userRouter);
+router.use("/playlists", playlistRouter);
+router.use("/users", authenticateJWT, userRouter);
 
 module.exports = router;
